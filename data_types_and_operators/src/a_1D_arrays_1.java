9175a00c31c1f1bb8644aa24a02f20322e0f091c
rmai <rmai.aickom@gmail.com>
2024-05-20 08:38:24 -0600

    create an one dimensional array

---
+++ data_types_and_operators/src/a_1D_arrays_1.java
@@ -0,0 +1,20 @@
+
+public class a_1D_arrays_1 {
+	// create a 1d array
+	public static void main(String[] args) {
+		int[] array1 = new int[3];
+
+		// value assignment
+		array1[0] = 2;
+		array1[1] = -10;
+		array1[2] = 432;
+
+		// print
+		for (int i = 0; i < 3; i++) {
+			System.out.println("The value of array [" + i + "] is " + array1[i]);
+		}
+
+		//
+
+	}
+}
