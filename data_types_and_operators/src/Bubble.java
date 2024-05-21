32bafbbe830403dcc05610eb532fcc9cbf0ad8b7
rmai <rmai.aickom@gmail.com>
2024-05-20 22:19:40 -0600

    add bubble.java

--- data_types_and_operators/src/Bubble.java
+++ data_types_and_operators/src/Bubble.java
@@ -19,14 +19,19 @@ public class Bubble {
 		// this is the Bubble sort
 		// TODO: algorithm
 		for (a = 1; a < size; a++) {
-			for (b = size - 1; b >= a; b--) {
+			for (b = size - 1; b >= a; b--) { // TODO: help me check if this is correct
+
 				if (nums[b - 1] > nums[b]) { // if out of order
 					// exchange elements
 					t = nums[b - 1];
 					nums[b - 1] = nums[b];
 					nums[b] = t;
+//					System.out.println("a is " + a + " and b is " + b + " t is " + t);
+					System.out.println(
+							String.format("a,b,t,nums[b-1], nums[b-1], %s %s %s %s %s", a, b, t, nums[b - 1], nums[b]));
 
 				}
+//				System.out.println("a: " + a + " and b is " + b + " t is " + t);
 			}
 		}
 		// display sorted array
